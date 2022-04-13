e7128aaa34844bccbec2f29700187a68f6e74b16
Rian Aquino <rian.aquino@outlook.com>
2022-04-13 20:21:14 -0300

    :lipstick: refactor: Mudança na lista de exibição de habilidades

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -68,18 +68,26 @@ export const ProfileContainer = styled.div`
             align-items: center;
             list-style: none;
             display: flex;
+            //background-color: #00000020;
+            //border-radius: 8px;
+            box-shadow: inset 0 40px 50px #00000020;
+            border-top: 4px solid ${props => props.theme.palette.primary.main};
+            padding: 4%;
+            margin-top: 4%;
 
             li {
                 border-bottom: 1px solid #00000020;
                 justify-content: space-between;
                 display: flex;
-                padding: 2% 4%;
+                padding: 2%;
                 width: 100%;
             }
         }
     }
 
     @media(min-width: 400px) {
+        //text-align: center;
+
         div#profile-container {
             flex-direction: row;
             
